2b04760d17eca0378d6019d6c83fe400a04087c5
sunny <jadebuddy717@gmail.com>
2023-07-12 10:43:33 +0530

    basic code

---
+++ dark/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import Main from './components/Main.js';
+import Navbar from './components/Navbar.js';
+
+function App() {
+  return (
+    <div className="container">
+            <Navbar />
+            <Main />
+    </div>
+  );
+}
+
+export default App;
