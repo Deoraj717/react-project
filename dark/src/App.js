28c5a23f1362df1ce7dde6dd4bda06874e54aa4b
sunny <jadebuddy717@gmail.com>
2023-07-12 11:00:47 +0530

    dark mode working fine

--- dark/src/App.js
+++ dark/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import Main from './components/Main.js';
 import Navbar from './components/Navbar.js';
+import React from 'react';
 
 function App() {
+
+const [darkMode,setDarkmode]=React.useState(false);
+    function toggleDarkMode(){
+        setDarkmode(prev=>!prev)
+    }
+
   return (
     <div className="container">
-            <Navbar />
-            <Main />
+            <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+            <Main darkMode={darkMode}/>
     </div>
   );
 }
